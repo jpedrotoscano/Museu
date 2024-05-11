5cd8bc61e8a55ef4e2954d5434b2b7eb59a3aaa8
jpedrotoscano <jpedro.toscano03@gmail.com>
2024-05-11 11:52:00 -0300

    fix

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
                 <Link to="/visitante" className="w-11 h-4rem text-2xl text-black-alpha-90 bg-blue-300 hover:bg-blue-200 border-round-2xl border-1 border-blue-500 hover:border-blue-50 cursor-pointer transition-duration-200 no-underline flex align-items-center justify-content-center font-medium shadow-1" style={{ fontFamily: "Roboto" }} >Acesse como Visitante</Link>
                 <div className="flex align-items-center jutify-content-center w-10 py-2">
                     <Divider className="text-black-alpha-90" type="solid"/>
-                    <span className="text-black-alpha-90 px-4">OU</span>
+                    <span className="text-black-alpha-90 px-4 font-bold">OU</span>
                     <Divider className="text-black-alpha-90" type="solid"/>
                 </div>
                 <Link to="/login" className="w-11 h-4rem text-2xl text-black-alpha-90 bg-blue-300 hover:bg-blue-200 border-round-2xl border-1 border-blue-500 hover:border-blue-50 cursor-pointer transition-duration-200 no-underline flex align-items-center justify-content-center font-medium shadow-1" style={{ fontFamily: "Roboto" }} >Entre como Admin</Link>
